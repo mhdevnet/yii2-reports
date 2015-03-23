201a81203911274fec5c3fe07a22635e6a396d91
ccsup <mpauL@callcentric.com>
2015-03-23 14:20:15 -0400

    Initial commit of yii2-reporting module

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace nitm\reports;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'nitm\reports\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace nitm\reports\controllers;
+
+use nitm\controllers\DefaultController;
+
+class DefaultController extends DefaultController
+{
+    public function actionIndex()
+    {
+        return $this->renderResponse();
+    }
+}
