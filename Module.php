3a028bfc5e9f17944614216b7d731ca19dc5459b
ccsup <mpauL@callcentric.com>
2015-10-29 12:53:50 -0400

    Added support for getUrls method

--- Module.php
+++ Module.php
@@ -5,7 +5,7 @@ namespace nitm\reports;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'nitm\reports\controllers';
-	
+
 	public $types = [];
 
     public function init()
@@ -14,7 +14,7 @@ class Module extends \yii\base\Module
 
         // custom initialization code goes here
     }
-	
+
 	public function getNavItems()
 	{
 		$ret_val = [];
@@ -27,12 +27,35 @@ class Module extends \yii\base\Module
 		}
 		return $ret_val;
 	}
-	
+
+	public function getUrls($id='nitm-reports')
+	{
+		return [
+            $id => $id,
+            $id . '/<controller:[\w\-]+>' => $id . '/<controller>/index',
+            $id . '/<controller:[\w\-]+>/<action:[\w\-]+>' => $id . '/<controller>/<action>',
+            $id . '/<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => $id . '/<controller>/<action>',
+            $id . '/<controller:[\w\-]+>/<action:[\w\-]+>/<type:\w+>' => $id . '/<controller>/<action>',
+			'<controller:(reports)>' => $id . '',
+			'<controller:(reports)>/<action>' => $id . '/default/<action>',
+			'<controller:(reports)>/<action>/<id:\d+>' => $id . '/default/<action>',
+			'<controller:(reports)>/<action>/<type:\w+>' => $id . '/default/<action>',
+        ];
+	}
+
+	protected function bootstrap($app)
+	{
+		/**
+		 * Setup urls
+		 */
+        $app->getUrlManager()->addRules($this->getUrls(), false);
+	}
+
 	public function getProjects()
 	{
 		return (new \nitm\helpers\Directory)->getDirectories('@runtime/reportico/projects');
 	}
-	
+
 	public function getReports($project)
 	{
 		return (new \nitm\helpers\Directory)->getFilesMatching('@runtime/reportico/projects/'.$project, ['xml']);
